850f5b4e551f08aeecae0be2d2532454b7778277
yuumens <ahmadrizki0209@gmail.com>
2024-01-02 14:03:02 +0700

    main : changes index.js script

--- UAP/index.js
+++ UAP/index.js
@@ -1,23 +1,44 @@
 function onSubmitPrices() {
-  document.getElementById("product-form");
-  document.addEventListener("submit", function (event) {
-    event.preventDefault();
+  const productCode = parseFloat(document.getElementById("product-code").value);
+  const productQuantity = parseFloat(
+    document.getElementById("product-quantity").value
+  );
 
-    const productCode = parseInt(document.getElementById("product-code").value);
-    const productQuantity = parseInt(
-      document.getElementById("product-quantity").value
-    );
-
-    if (productCode === 0o1) {
-      const price = 10.0;
-      const productSubtotal = price * productQuantity;
-      alert("total pembayaran adalah : " + productSubtotal);
+  if (productCode === 0o1) {
+    const price = 10000;
+    const productSubtotal = price * productQuantity;
+    alert("total pembayaran adalah : " + productSubtotal);
+    const userCash = parseFloat(prompt("Masukkan jumlah uang:"));
+    if (isNaN(userCash)) {
+      alert("Input tidak valid, silakan masukkan angka.");
+    } else {
+      const totalChange = userCash - productSubtotal;
+      alert("kembalian anda adalah : " + totalChange);
     }
-    // Reset the form
-    document.getElementById("product-code").reset();
-    document.getElementById("product-quantity").reset();
-  });
+  } else if (productCode === 0o2) {
+    const price = 20000;
+    const productSubtotal = price * productQuantity;
+    alert("total pembayaran adalah : " + productSubtotal);
+    const userCash = parseFloat(prompt("Masukkan jumlah uang:"));
+    if (isNaN(userCash)) {
+      alert("Input tidak valid, silakan masukkan angka.");
+    } else {
+      const totalChange = userCash - productSubtotal;
+      alert("kembalian anda adalah : " + totalChange);
+    }
+  } else if (productCode === 0o3) {
+    const price = 30000;
+    const productSubtotal = price * productQuantity;
+    alert("total pembayaran adalah : " + productSubtotal);
+    const userCash = parseFloat(prompt("Masukkan jumlah uang:"));
+    if (isNaN(userCash)) {
+      alert("Input tidak valid, silakan masukkan angka.");
+    } else {
+      const totalChange = userCash - productSubtotal;
+      alert("kembalian anda adalah : " + totalChange);
+    }
+  } else {
+    alert("Product Code Tidak Di Temukan");
+  }
+  document.getElementById("product-form").reset();
 }
-
-//   function onSubmitPrice(){asd
-//   }
